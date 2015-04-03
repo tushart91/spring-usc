855808f336bd4eba7920a454b4b34906ea396cdf
Tushar Tiwari <tushart91@gmail.com>
2015-04-02 18:05:53 -0700

    Final Working

--- Information Integration/Homework/HW10/HW10/src/main/java/edu/usc/hw10/Program.java
+++ Information Integration/Homework/HW10/HW10/src/main/java/edu/usc/hw10/Program.java
@@ -1,11 +1,20 @@
 package edu.usc.hw10;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -17,13 +26,63 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Program obj = new Program();
-		LinkedHashMap<String, Integer> d1 = obj.get_d("data/d1.csv", N);
-		LinkedHashMap<String, Integer> d2 = obj.get_d("data/d2.csv", N);
 		
+		//Initialize data structure
+		HashMap<String, Integer> d1 = obj.get_d("data/d1.csv", N);
+		HashMap<String, Integer> d2 = obj.get_d("data/d2.csv", N);
+		LinkedHashMap<String, String> result = obj.get_results();
+		HashMap<String, String> gt = obj.get_groundtruth();
+		
+		//Initialize
+		File file = null;
+		FileOutputStream fout = null;
+		BufferedWriter out = null;
+		try {
+			file = new File("output.csv");
+			fout = new FileOutputStream(file);
+			out = new BufferedWriter(new OutputStreamWriter(fout));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		//Line number for output
+		String str = null;
+		Float count = 0f;
+		Iterator<String> it = result.keySet().iterator();
+		try{
+			out.write("d1,d2\n");
+			System.out.println("No matches for:");
+			while(it.hasNext()) {
+				str = it.next();
+				
+				out.write(d1.get(str) + "," + d2.get(result.get(str)) + "\n");
+				
+				//count for true positive matches
+				if (result.get(str).equalsIgnoreCase(gt.get(str)))
+					count++;
+				else {
+					System.out.println(str + " : " + result.get(str) + " - " + gt.get(str));
+					System.out.println();
+				}
+				
+			}
+			out.close();
+			fout.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(count.toString() + " true positive matches");
+		//Compute F-Score
+		Float precision = count / result.size();
+		System.out.println("Precision: " + precision);
+		Float recall = count / gt.size();
+		System.out.println("Recall: " + recall);
+		Float fscore = 2 * ((precision * recall)/(precision + recall));
+		System.out.println("F-Score: " + fscore);
 	}
 
-	public LinkedHashMap<String, Integer> get_d(String fileName, int N) {
-		LinkedHashMap<String, Integer> d = new LinkedHashMap<String, Integer>();
+	public HashMap<String, Integer> get_d(String fileName, int N) {
+		HashMap<String, Integer> d = new HashMap<String, Integer>();
 
 		// Get file from resources folder
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -37,7 +96,7 @@ public class Program {
 			reader.readLine();
 			while ((line = reader.readLine()) != null) {
 				parts = line.split(",", 4);
-				d.put(parts[NAME].toLowerCase(), i++);
+				d.put(parts[NAME].trim().toLowerCase(), i++);
 			}
 			reader.close();
 			inputStream.close();
@@ -47,25 +106,24 @@ public class Program {
 		return d;
 	}
 
-	public HashMap<String, String> get_result() {
+	public LinkedHashMap<String, String> get_results() {
 
 		//Initialize data structure
-		HashMap<String, String> map = new HashMap<String, String>();
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
 		
 		// Get file from resources folder
 		ClassLoader classLoader = getClass().getClassLoader();
-		InputStream inputStream = classLoader.getResourceAsStream("results.csv");
+		InputStream inputStream = classLoader.getResourceAsStream("data/results.csv");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				inputStream));
 		String line = "";
 		String[] parts = null;
-		
 		//Read results.csv
 		try {
 			reader.readLine();
 			while ((line = reader.readLine()) != null) {
-				parts = line.split(",", 8);
-				map.put(parts[0].toLowerCase(), parts[4].toLowerCase());
+				parts = line.replace("\"", "").split(",", 9);
+				map.put(parts[4].toLowerCase(), parts[0].toLowerCase());
 			}
 			reader.close();
 			inputStream.close();
@@ -82,22 +140,22 @@ public class Program {
 		
 		// Get file from resources folder
 		ClassLoader classLoader = getClass().getClassLoader();
-		InputStream inputStream = classLoader.getResourceAsStream("groundtruth.csv");
+		InputStream inputStream = classLoader.getResourceAsStream("data/groundtruth.csv");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				inputStream));
 		String line = "";
 		String[] parts = null;
 		String key = null;
 		
-		//Read results.csv
+		//Read groundtruth.csv
 		try {
 			reader.readLine();
 			while ((line = reader.readLine()) != null) {
-				parts = line.split(",", 8);
+				parts = line.split(",", 4);
 				key = parts[0].toLowerCase();
 				line = reader.readLine();
-				parts = line.split(",", 8);
-				map.put(key, parts[0].toLowerCase());
+				parts = line.split(",", 4);
+				map.put(parts[0].toLowerCase(), key);
 			}
 			reader.close();
 			inputStream.close();
